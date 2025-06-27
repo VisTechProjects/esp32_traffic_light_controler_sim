a8ef0969f42f40674cb47c19fe71292732de0dff
VisK <41296910+VisTechProjects@users.noreply.github.com>
2025-06-26 22:13:05 -0400

    Update config.h

--- src/config.h
+++ src/config.h
@@ -1,9 +1,9 @@
 #define VERSION "0.1"
 
 // pins for relay board
-#define LED_red_pin 16
-#define LED_yellow_pin 17
-#define LED_green_pin 18
+#define LED_red_pin 12
+#define LED_yellow_pin 14
+#define LED_green_pin 27
 
 unsigned long blinkInterval = 1000;        // Traffic light blink mode interval in milliseconds
 const unsigned long dangerHoldTime = 3000; // How long you must stay in the danger zone before flashing (3s)
